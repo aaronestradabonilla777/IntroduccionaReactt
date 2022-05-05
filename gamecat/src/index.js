fba03d28af43c4cd562568d5e8e5b8abae12f365
Haluzdarck <104384350+aaronestradabonilla777@users.noreply.github.com>
2022-05-05 16:48:19 -0700

    se usara en la sintaxis de funciones  una flecha

--- gamecat/src/index.js
+++ gamecat/src/index.js
@@ -6,12 +6,12 @@ import './index.css';
 class Square extends React.Component {
   render() {
     return (
-      <button className="square" onClick={function() { console.log('click'); }}>
+      <button className="square" onClick={() => console.log('click')}>
         {this.props.value}
       </button>
     );
   }
-}
+ }
 
 class Board extends React.Component {
   renderSquare(i) {
