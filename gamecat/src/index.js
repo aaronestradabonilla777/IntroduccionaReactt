954e425b3942704bb99c5e8ea8b9d94a427f11ac
Haluzdarck <104384350+aaronestradabonilla777@users.noreply.github.com>
2022-05-05 16:47:06 -0700

    rellenando el componente square

--- gamecat/src/index.js
+++ gamecat/src/index.js
@@ -2,29 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
 class Square extends React.Component {
   render() {
     return (
-      <button className="square">
+      <button className="square" onClick={function() { console.log('click'); }}>
         {this.props.value}
       </button>
     );
   }
 }
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+class Board extends React.Component {
+  renderSquare(i) {
+    return (
+      <Square
+        value={this.props.squares[i]}
+        onClick={() => this.props.onClick(i)}
+      />
+    );
+    }}
