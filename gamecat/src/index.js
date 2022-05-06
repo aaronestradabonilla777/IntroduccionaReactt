6a716bff4b2d14361bb3799548c54c3113cdcb8b
Haluzdarck <104384350+aaronestradabonilla777@users.noreply.github.com>
2022-05-05 17:15:15 -0700

    se actualiza la funcion handleclick del comp board

--- gamecat/src/index.js
+++ gamecat/src/index.js
@@ -23,8 +23,12 @@ class Board extends React.Component {
 
   handleClick(i) {
     const squares = this.state.squares.slice();
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
     squares[i] = 'X';
-    this.setState({squares: squares});
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext,
+    });
   }
 
   renderSquare(i) {
