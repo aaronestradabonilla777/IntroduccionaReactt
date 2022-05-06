34c0eaa907c5bd461f53068c721f87547aa67b67
Haluzdarck <104384350+aaronestradabonilla777@users.noreply.github.com>
2022-05-05 17:42:37 -0700

    se  modifica handleclick

--- gamecat/src/index.js
+++ gamecat/src/index.js
@@ -66,7 +66,7 @@ class Game extends React.Component {
   }
 
   handleClick(i) {
-    const history = this.state.history;
+    const history = this.state.history.slice(0, this.state.stepNumber + 1);
     const current = history[history.length - 1];
     const squares = current.squares.slice();
     if (calculateWinner(squares) || squares[i]) {
@@ -77,10 +77,11 @@ class Game extends React.Component {
       history: history.concat([{
         squares: squares,
       }]),
+      stepNumber: history.length,
       xIsNext: !this.state.xIsNext,
     });
   }
-  
+
 
   jumpTo(step) {
     this.setState({
