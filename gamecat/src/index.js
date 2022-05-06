6df4d17a63e0483012c41fa696b577656513d5a0
Haluzdarck <104384350+aaronestradabonilla777@users.noreply.github.com>
2022-05-05 17:04:37 -0700

    eliminamos el constructor de square y mas cambios

--- gamecat/src/index.js
+++ gamecat/src/index.js
@@ -5,24 +5,17 @@ import './index.css';
 
 
 class Square extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: null,
-    };
-  }
-
   render() {
     return (
-      <button 
-      className="square"
-        onClick={() => this.setState({value: 'X'})}
+      <button
+        className="square"
+        onClick={() => this.props.onClick()}
       >
-        {this.state.value}
+        {this.props.value}
       </button>
     );
   }
- }
+}
 
  class Board extends React.Component {
   constructor(props) {
